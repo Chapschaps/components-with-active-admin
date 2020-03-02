db98933f1ebf24127f6d63e3ef68117554af1a1f
Leo Chaput <leo.chaput@hotmail.fr>
2020-03-02 12:54:55 +0100

    migrations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :components]
 
   def home
+    @projects = Project.all.order('priority desc')
+  end
+
+  def components
+    @projects = Project.all.order('priority desc')
   end
 end
---
+++ app/models/project.rb
@@ -0,0 +1,9 @@
+class Project < ApplicationRecord
+  has_many_attached :photos
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
+  validates :client, presence: true
+  validates :priority, presence: true, inclusion: { in: (1..20).to_a }
+  belongs_to :user
+end
